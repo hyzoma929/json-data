2183ecba722b19f6947a0e0515205bf2b1b0fc3d
hyzoma929 <49223966+hyzoma929@users.noreply.github.com>
2024-10-15 09:30:22 +0700

    Create voc.js

---
+++ voc.js
@@ -0,0 +1,12 @@
+[
+    {
+        voucherCode: "VG3ED4XHR5",
+        voucherName: "Vixal Kuat Segar 600ml Potongan Rp8.000",
+        merchantName: "Indomaret"
+    },
+    {
+        voucherCode: "VG3DF5G362",
+        voucherName: "Vixal Kuat Segar 600ml Potongan Rp8.000",
+        merchantName: "Indomaret"
+    }
+]
